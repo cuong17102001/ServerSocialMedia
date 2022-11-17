70a54dd6d203ed5eda6e57321f974e51db3a3de6
nqcuong <nqcuong.1710@gmail.com>
2022-11-17 12:06:39 +0700

    nothing

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -110,3 +110,16 @@ export const unFollowUser = async(req , res)=>{
         }
     }
 }
+
+export const getAllUser = async(req , res)=>{
+    try {
+        let users = await UserModel.find()
+        users = users.map((user) =>{
+            const {password , ...otherDetails} = user._doc
+            return otherDetails
+        })
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
