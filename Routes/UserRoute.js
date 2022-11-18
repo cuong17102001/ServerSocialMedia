8e4cb3c383fe025474552288ebcecfbd7ce73b16
nqcuong <nqcuong.1710@gmail.com>
2022-11-18 19:28:50 +0700

    make it better

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -3,7 +3,7 @@ import { deleteUser, followUser, getUser, unFollowUser, updateUser ,getAllUser }
 
 const router = express.Router();
 
-router.get('/:id' , getAllUser);
+router.get('/:id/allUser' , getAllUser);
 router.get('/:id' , getUser);
 router.put('/:id' , updateUser);
 router.delete('/:id' , deleteUser)
